1ed26c9d1b1b2c6a0f097657c23503271b02245e
George Politis <sitilop@hotmail.com>
2016-04-11 19:03:36 +0300

    Initial commit

---
+++ src/Map.java
@@ -0,0 +1,29 @@
+/**
+ * Created by George, Haris and Hronis on 11-Apr-16.
+ */
+public class Map
+{
+    int [][] arrayMap;
+
+    public Map()
+    {
+        // [nest rank], [coordinates], [number of hornets
+        arrayMap = new int[12][3];
+    }
+
+    public Map(int x,int y)
+    {
+     arrayMap = new int[x][y];
+    }
+
+    public void distanceMax()
+    {
+
+
+    }
+
+
+
+
+
+}
