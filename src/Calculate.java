2b7f45468b860d700fe815e43aaa0cca200a9484
George Politis <sitilop@hotmail.com>
2016-04-12 03:11:50 +0300

    interface

---
+++ src/Calculate.java
@@ -0,0 +1,9 @@
+/**
+ * Created by George on 12-Apr-16.
+ */
+public interface Calculate
+{
+    public void map(int x, int y);
+    public void distanceMax(); // calculaters dmax among all nests
+    public int penalty(); // calculates if kiled hornets have been counted more than once
+}
