89c1d80e330787db7a41ffbaa88f60a25294cf99
George Politis <sitilop@hotmail.com>
2016-04-25 20:31:52 +0300

    Monday- Tournament Selection - CrossOver

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Random;
 import org.w3c.dom.Document;
 
@@ -39,28 +40,74 @@ public class Main
 
     public static void main(String[] args)
     {
-        int maxGenerations = 500;
+        int maxGenerations = 10;
         int population = 50;
         int chromosome[][] = new int[population][6]; // array with  chromosome
+        int roulleteChromosome[][] = new int[population][6];
         Random rand = new Random();
         Map map1 = new Map();
         int map [][] = map1.getArrayMap();
-        int fitness[]= new int[population];
+       int fitness[]= new int[population];
 
-
+        int roulleteSelected ;
+        int tournamentSelected;
 
         for(int i=0;i<population;i++)
         {
             for(int j=0;j<6;j++)
             {
                 chromosome[i][j]= rand.nextInt((100-1)+1)+1;
-//                System.out.print(chromosome[i][j]+"\t");
+                System.out.print(chromosome[i][j]+"\t");
             }
+            System.out.println();
         }// end for
 
         for (int k=0; k< maxGenerations;k++ ) {
 
+            System.out.println("Generation: " + (k+1)+"--------------");
+
+
+
+            //System.out.println("Fitness is happening now Dude!!!");
+
             fitness = Utilities.CalculateFitness(chromosome,population,map,map1.getDistaneMax());
+
+
+            for(int i=0;i<population;i++)
+            {
+                tournamentSelected= Utilities.tournamentSelection(fitness);
+
+                //  System.out.println("The roullete wheel select: "+roulleteSelected);
+                // System.out.print("The new array Chromosome: ");
+                for(int j=0;j<6;j++)
+                {
+                    roulleteChromosome[i][j] =chromosome[tournamentSelected][j];
+                     System.out.print(roulleteChromosome[i][j]+" , ");
+
+                }
+                System.out.println();
+
+
+            }// end for
+           /* for(int i=0;i<population;i++)
+            {
+                roulleteSelected= Utilities.roulleteSelectionMethod(fitness);
+
+              //  System.out.println("The roullete wheel select: "+roulleteSelected);
+                // System.out.print("The new array Chromosome: ");
+                for(int j=0;j<6;j++)
+                {
+                    roulleteChromosome[i][j] =chromosome[roulleteSelected][j];
+                   // aisto dialo System.out.print(roulleteChromosome[i][j]+" , ");
+
+                }
+                //System.out.println(fitness[roulleteSelected]);
+
+
+            }// end for
+            */
+            chromosome=Utilities.crossOver(roulleteChromosome,population);
+            // chromosome=roulleteChromosome;
 
 
         }
--- src/Utilities.java
+++ src/Utilities.java
@@ -1,3 +1,7 @@
+
+import java.util.Random;
+import java.util.Set;
+
 /**
  * Created by George on 23-Apr-16.
  */
@@ -9,12 +13,12 @@ public abstract class Utilities
     public static int [] CalculateFitness(int [][] coordinates,int population,int map[][],double maxD)
     {
 
-        double d=0;
-        int deadHornets[] = new int[50];
-        int k =0;
+        double d;
+        int deadHornets[] = new int[population];
+        int k ;
         for (int i=0; i< population ;i++ )
         {
-            System.out.println("Population "+i);
+           // System.out.println("Population "+i);
             for (int j=0; j < 6 ;j+=2 ) {
 
                 for (int i1 = 0; i1 < map.length; i1++)
@@ -27,7 +31,13 @@ public abstract class Utilities
                    map[i1][2] -=k;
                     if(map[i1][2]<0)
                     {
-                        deadHornets[i] -= deadHornets[i]*(20/100);
+
+                       // System.out.println("Problem with the: "+i);
+                       // System.out.println("DeadHornets First: "+ deadHornets[i]);
+                        deadHornets[i] = deadHornets[i] -((deadHornets[i]*20)/100);
+                     //   System.out.println("DeadHornets During: "+ deadHornets[i]+",k:"+ k);
+                        deadHornets[i] += k;
+                       //System.out.println("DeadHornets total: "+ deadHornets[i]);
                        continue;
                     }
 
@@ -37,7 +47,8 @@ public abstract class Utilities
                 }
             }
 
-            System.out.println("DeadHornets : "+deadHornets[i]);
+            //System.out.println("DeadHornets : "+deadHornets[i]);
+
             //1st Sollution
           //  map = refill(map);
 
@@ -55,7 +66,118 @@ public abstract class Utilities
         }
         return deadHornets;
     }
+    public static int roulleteSelectionMethod(int fitness[])
+    {
 
+        int totalSum = 0;
+        Random rand = new Random();
+        for(int i=0;i<fitness.length;i++)
+        {
+            totalSum += fitness[i];
+        }
+        int randNumber =  rand.nextInt((totalSum)+1);
+
+        int partialSum = 0;
+
+
+        for(int i=0;i<fitness.length;i++)
+        {
+
+           // randNumber -= fitness[i];
+            //if(randNumber<=0) return i;
+
+            partialSum +=fitness[i];
+            if(partialSum>=randNumber)
+            {
+                return i;
+            }
+
+        }
+
+        return fitness.length-1;
+
+    }
+    public static int tournamentSelection(int fitness[])
+    {
+
+        Random rand = new Random();
+        int randSize = rand.nextInt(fitness.length)+1;
+        int bestChromosome = -999;
+        int bestIndex=0;
+        int tournament [] = new int[randSize];
+        int randIndex;
+        for(int i =0;i<randSize;i++)
+        {
+            randIndex=rand.nextInt(fitness.length-1)+1;
+            tournament[i]=fitness[randIndex];
+            if(tournament[i]>bestChromosome)
+            {
+               bestChromosome = tournament[i];
+                bestIndex = randIndex;
+
+            }
+        }
+          return bestIndex;
+    }
+
+    public static int [][] crossOver(int [][]roulleteChromosome,int population)
+    {
+      Random rand = new Random();
+        int randFather,randMother,randCut;
+        int childrenChromosome [][] = new int[population][6];
+        randCut = 3;//3  ((rand.nextInt(2)+1)*2)-1;//emfanizei to 1 h to 3.
+       // System.out.println(randCut);
+
+        for(int i=0;i<population/2;i++)
+        {
+            randFather = rand.nextInt((population-1)+1);
+            randMother = rand.nextInt((population-1)+1);
+
+
+            for(int i2=0;i2<population-1;i2+=2)
+            {
+                for(int i1=0;i1<=randCut;i1++)
+                {
+                    childrenChromosome[i2][i1] = roulleteChromosome[randFather][i1];
+                     childrenChromosome[i2+1][i1] = roulleteChromosome[randMother][i1];
+
+
+                }
+                for (int j=randCut+1; j<6; j++)
+                {
+                    childrenChromosome[i2][j] = roulleteChromosome[randMother][j];
+                    childrenChromosome[i2+1][j] = roulleteChromosome[randFather][j];
+
+                }
+
+
+                System.out.println("\nFather");
+                          for(int k=0; k< 6; k++)
+                          {
+                            System.out.print(roulleteChromosome[randFather][k]+",");
+                          }
+                System.out.println("\nMother");
+                for(int k=0; k< 6; k++)
+                {
+                    System.out.print(roulleteChromosome[randMother][k]+",");
+                }
+                  /*
+                System.out.println("\nChild1");
+                for(int k=0; k< 6; k++)
+                {
+                    System.out.print( childrenChromosome[i2][k] +",");
+                }
+                System.out.println("\nChild2");
+                for(int k=0; k< 6; k++)
+                {
+                    System.out.print( childrenChromosome[i2+1][k] +",");
+                }
+               */
+            }
+
+        }
+        return childrenChromosome;
+    }
     public static int[][] refill(int arrayMap [][])
     {
         for(int i=0;i<arrayHornetsPopulation.length;i++)
